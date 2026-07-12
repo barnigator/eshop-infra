fe006c3e5ad0ebda7e6320ba198a749bc42db843
barnigator <lorivan@bk.ru>
2026-07-12 19:14:09 +0300

    postgres init

---
+++ postgres/init/01-create-database.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+psql \
+  --username "$POSTGRES_USER" \
+  --dbname "$POSTGRES_DB" \
+  --set=ON_ERROR_STOP=1 \
+  --set=seller_user="$ESHOP_SELLER_DB_USER" \
+  --set=seller_password="$ESHOP_SELLER_DB_PASSWORD" \
+  --set=sso_user="$ESHOP_SSO_DB_USER" \
+  --set=sso_password="$ESHOP_SSO_DB_PASSWORD" <<-'EOSQL'
+    CREATE USER :"seller_user" WITH PASSWORD :'seller_password';
+    CREATE DATABASE eshop_seller OWNER :"seller_user";
+
+    CREATE USER :"sso_user" WITH PASSWORD :'sso_password';
+    CREATE DATABASE eshop_sso OWNER :"sso_user";
+EOSQL
