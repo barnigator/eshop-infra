461d7e2546ad256116c818d4628d2268c8d30be9
barnigator <lorivan@bk.ru>
2026-07-30 18:33:42 +0300

    comands

--- postgres/init/01-create-database.sh
+++ postgres/init/01-create-database.sh
@@ -7,12 +7,15 @@ psql \
   --set=ON_ERROR_STOP=1 \
   --set=seller_user="$ESHOP_SELLER_DB_USER" \
   --set=seller_password="$ESHOP_SELLER_DB_PASSWORD" \
+  --set=seller_db="$ESHOP_SELLER_DB_NAME" \
+  --set=seller_test_db="$ESHOP_SELLER_TEST_DB_NAME" \
   --set=sso_user="$ESHOP_SSO_DB_USER" \
-  --set=sso_password="$ESHOP_SSO_DB_PASSWORD" <<-'EOSQL'
+  --set=sso_password="$ESHOP_SSO_DB_PASSWORD" \
+  --set=sso_db="$ESHOP_SSO_DB_NAME" <<-'EOSQL'
     CREATE USER :"seller_user" WITH PASSWORD :'seller_password';
-    CREATE DATABASE eshop_seller OWNER :"seller_user";
-    CREATE DATABASE eshop_seller_test OWNER :"seller_user";
+    CREATE DATABASE :"seller_db" OWNER :"seller_user";
+    CREATE DATABASE :"seller_test_db" OWNER :"seller_user";
 
     CREATE USER :"sso_user" WITH PASSWORD :'sso_password';
-    CREATE DATABASE eshop_sso OWNER :"sso_user";
+    CREATE DATABASE :"sso_db" OWNER :"sso_user";
 EOSQL
